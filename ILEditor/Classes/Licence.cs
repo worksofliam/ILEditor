0bdbb8f7e22191eb7a3db8647eef9b3af7b2f36a
Liam Allan <WorksOfLiam@users.noreply.github.com>
2017-09-08 11:17:10 +0100

    Subscription licence keys

--- ILEditor/Classes/Licence.cs
+++ ILEditor/Classes/Licence.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,117 +10,73 @@ namespace ILEditor.Classes
 {
     class Licence
     {
-        public static readonly Dictionary<char, string> Keys = new Dictionary<char, string>()
-        {
-            { '1', "Y656" },
-            { '2', "Wltq" },
-            { '3', "Ww4C" },
-            { '4', "DEPl" },
-            { '5', "xTpb" },
-            { '6', "6qYi" },
-            { '7', "UTzm" },
-            { '8', "7KH5" },
-            { '9', "1Ofq" },
-            { '0', "ar0v" },
-            { 'A', "3T7c" },
-            { 'B', "iz5O" },
-            { 'C', "Rq1G" },
-            { 'D', "PZ2q" },
-            { 'E', "kCVH" },
-            { 'F', "ktZj" },
-            { 'G', "9VNc" },
-            { 'H', "c3WK" },
-            { 'I', "Mnb7" },
-            { 'J', "sicj" },
-            { 'K', "pSlC" },
-            { 'L', "Sa8R" },
-            { 'M', "1QuZ" },
-            { 'N', "mhF0" },
-            { 'O', "k89s" },
-            { 'P', "c6hq" },
-            { 'Q', "I8KG" },
-            { 'R', "FsQP" },
-            { 'S', "20lH" },
-            { 'T', "at9Z" },
-            { 'U', "TPF3" },
-            { 'V', "Dktf" },
-            { 'W', "KZZ9" },
-            { 'X', "qnFU" },
-            { 'Y', "BX8r" },
-            { 'Z', "rfBm" },
-            { 'a', "eNI4" },
-            { 'b', "N2bD" },
-            { 'c', "bEI6" },
-            { 'd', "PFPC" },
-            { 'e', "5gy0" },
-            { 'f', "MRlh" },
-            { 'g', "UgEv" },
-            { 'h', "nHdU" },
-            { 'i', "7pSr" },
-            { 'j', "bfZi" },
-            { 'k', "jZQu" },
-            { 'l', "OFA6" },
-            { 'm', "xbiN" },
-            { 'n', "hZwB" },
-            { 'o', "jpln" },
-            { 'p', "VtRl" },
-            { 'q', "OSKg" },
-            { 'r', "CalW" },
-            { 's', "4cyc" },
-            { 't', "FFnY" },
-            { 'u', "BLay" },
-            { 'v', "DGV8" },
-            { 'w', "OGrm" },
-            { 'x', "41XV" },
-            { 'y', "L6Jo" },
-            { 'z', "bItZ" }
-        };
-
-        //XXXXX-XXXX-XXXX-XXXX-XXXX-XXXX
+        
         public static Boolean IsValid(string Key)
         {
-            String[] Parts;
-            List<String> EncKeys = new List<string>();
-            if (Key.Length == 30)
+            RegistryKey licenceinfo = Registry.CurrentUser.CreateSubKey("idle");
+            Boolean result = false;
+            double unixTime = 0;
+            DateTime dtDateTime;
+            try
             {
-                Parts = Key.Split('-');
-                if (Parts.Length == 6)
+                using (WebClient client = new WebClient())
                 {
-                    foreach(char c in Parts[0].ToCharArray())
+                    string url = "http://worksofbarry.com/idle/valid.php?k=" + Key + "&u=" + Environment.MachineName;
+                    string strout = client.DownloadString(url);
+                    if (strout != "false")
                     {
-                        EncKeys.Add(Keys[c]);
+                        if (Double.TryParse(strout, out unixTime) == false)
+                            throw new Exception("Not a number");
+
+                        dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+                        dtDateTime = dtDateTime.AddSeconds(unixTime).ToLocalTime();
+
+                        licenceinfo.SetValue("ts", dtDateTime);
+                        if (DateTime.Now < dtDateTime)
+                        {
+                            result = true;
+                        }
+                        licenceinfo.SetValue("key", Key);
                     }
-                    for(int i = 0; i < 5; i++)
-                    {
-                        if (Parts[i+1] != EncKeys[i])
-                            return false;
-                    }
-                    
-                    RegistryKey licencekey = Registry.CurrentUser.CreateSubKey("ileditor");
-                    licencekey.SetValue("key", Key);
-                    licencekey.Close();
-                    return true;
                 }
-                else
-                    return false;
             }
-            else
-                return false;
+            catch 
+            {
+                if (licenceinfo.GetValue("ts") != null)
+                {
+                    dtDateTime = (DateTime)licenceinfo.GetValue("ts");
+                    if (DateTime.Now < dtDateTime)
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        licenceinfo.DeleteValue("ts");
+                    }
+
+                }
+            }
+
+            licenceinfo.Close();
+
+            return result;
         }
 
         public static Boolean CheckExistsIsValid()
         {
+            RegistryKey licenceinfo = Registry.CurrentUser.CreateSubKey("idle");
+
             Boolean result = false;
             string key = "";
-            RegistryKey licencekey = Registry.CurrentUser.CreateSubKey("ileditor");
-            if (licencekey.GetValue("key") != null)
+            
+            if (licenceinfo.GetValue("key") != null)
             {
-                key = licencekey.GetValue("key").ToString();
+                key = licenceinfo.GetValue("key").ToString();
                 if (IsValid(key))
                     result = true;
             }
 
-            licencekey.Close();
+            licenceinfo.Close();
 
             return result;
         }
