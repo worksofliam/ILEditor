5e828f3f2cc10d33df6bdbb48ccac5471f90877a
Liam Allan <WorksOfLiam@users.noreply.github.com>
2017-11-07 22:36:13 +0000

    AutoCompleteData Class

---
+++ ILEditor/Classes/AutoCompleteData.cs
@@ -0,0 +1,42 @@
+﻿using ICSharpCode.AvalonEdit.CodeCompletion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+using System.Windows.Media;
+
+namespace ILEditor.Classes
+{
+    class AutoCompleteData : ICompletionData
+    {
+        public AutoCompleteData(string text, string desc)
+        {
+            this.Text = text;
+            this.Description = desc;
+        }
+
+        public System.Windows.Media.ImageSource Image
+        {
+            get { return null; }
+        }
+
+        public string Text { get; private set; }
+        public object Description { get; private set; }
+
+        // Use this property if you want to show a fancy UIElement in the list.
+        public object Content
+        {
+            get { return this.Text; }
+        }
+
+        public double Priority => 1;
+
+        public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
+        {
+            textArea.Document.Replace(completionSegment, this.Text);
+        }
+    }
+}
