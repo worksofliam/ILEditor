0a1a721439b821e0a0382dfa247d2004fd5a6a01
Liam Allan <WorksOfLiam@users.noreply.github.com>
2018-02-16 21:40:42 +0000

    Update button in outline

--- ILEditor/UserTools/OutlineView.Designer.cs
+++ ILEditor/UserTools/OutlineView.Designer.cs
@@ -32,6 +32,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OutlineView));
             this.funcList = new System.Windows.Forms.TreeView();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.updateButton = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // funcList
@@ -43,7 +48,7 @@
             this.funcList.Location = new System.Drawing.Point(0, 0);
             this.funcList.Name = "funcList";
             this.funcList.SelectedImageIndex = 0;
-            this.funcList.Size = new System.Drawing.Size(284, 261);
+            this.funcList.Size = new System.Drawing.Size(284, 236);
             this.funcList.TabIndex = 0;
             this.funcList.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.funcList_NodeMouseDoubleClick);
             // 
@@ -58,16 +63,49 @@
             this.imageList1.Images.SetKeyName(4, "diagram.png");
             this.imageList1.Images.SetKeyName(5, "compile.png");
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // updateButton
+            // 
+            this.updateButton.Image = global::ILEditor.Properties.Resources.refresh;
+            this.updateButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(103, 22);
+            this.updateButton.Text = "Update Listing";
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.funcList);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 236);
+            this.panel1.TabIndex = 2;
+            // 
             // OutlineView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.CloseButton = false;
-            this.Controls.Add(this.funcList);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "OutlineView";
             this.Text = "Outline View";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,5 +113,8 @@
 
         private System.Windows.Forms.TreeView funcList;
         private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton updateButton;
+        private System.Windows.Forms.Panel panel1;
     }
 }
